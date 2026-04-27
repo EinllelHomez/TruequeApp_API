58bbc14f6cc6cd30f88e073712a0046c97b0a376
Samydap <samueldavid.cerquera.p@gmail.com>
2026-04-27 16:32:03 -0500

    Agregar límite longitud a campos de Categoria

--- models/Categoria.js
+++ models/Categoria.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const categoriaSchema = new mongoose.Schema({
-  nombre:      { type: String, required: true, unique: true, trim: true },
-  descripcion: { type: String, trim: true },
+  nombre:      { type: String, required: true, unique: true, trim: true, maxlength: 100 },
+  descripcion: { type: String, trim: true, maxlength: 100 },
   activo:      { type: Boolean, default: true }
 }, { timestamps: true });
 
