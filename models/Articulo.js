9654e468d72e3a24a93b8f0fe5abb9f18c425c99
Samydap <samueldavid.cerquera.p@gmail.com>
2026-04-27 16:30:41 -0500

    Agregar límites de longitud a campos de Articulo

--- models/Articulo.js
+++ models/Articulo.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const ESTADOS = ['nuevo', 'bueno', 'regular', 'malo'];
 
 const articuloSchema = new mongoose.Schema({
-  titulo:      { type: String, required: true, trim: true },
-  descripcion: { type: String, required: true }, 
+  titulo:      { type: String, required: true, trim: true, maxlength: 100 },
+  descripcion: { type: String, required: true, maxlength: 100 }, 
   estado:      { type: String, enum:ESTADOS, default: 'bueno' },
   imagenes:    [{ type: String }],
   disponible:  { type: Boolean, default: true },
