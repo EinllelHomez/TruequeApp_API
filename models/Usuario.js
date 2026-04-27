109127ae4a692df0b5a2d435f3b5934d7e783699
Samydap <samueldavid.cerquera.p@gmail.com>
2026-04-27 15:38:18 -0500

    Definir estructura y funciones js, clase Usuario

---
+++ models/Usuario.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: { type: String, required: true, trim: true },
+  email:  { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true, minlength: 6 },
+  telefono: { type: String, trim: true },
+  direccion: { type: String, trim: true },
+  foto: { type: String, default: '' },
+  activo: { type: Boolean, default: true }
+}, { timestamps: true });
+
+usuarioSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+
+usuarioSchema.methods.compararPassword = async function (passwordIngresado) {
+  return await bcrypt.compare(passwordIngresado, this.password);
+};
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
